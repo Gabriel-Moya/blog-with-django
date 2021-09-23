ca5cb1baa1c5d6d1e16c442081b6f14b024c9e09
Gabriel Moya <gabrielmoya123@gmail.com>
2021-09-23 18:07:09 -0400

    Add templates for posts

--- blogDjango/blog/models.py
+++ blogDjango/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Post(models.Model):
@@ -10,5 +11,11 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ("-created",)
+
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("blog:detail", kwargs={"slug": self.slug})
